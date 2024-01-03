58f8b77dd55ae1f5adaf0819c32d17e04d7dd528
matoka02 <a.m.nigomedzyanova@gmail.com>
2024-01-03 16:12:43 +0200

    create

---
+++ calculator.js
@@ -0,0 +1,88 @@
+const sum = (a, b) => {
+  return a + b;
+};
+
+const minus = (a, b) => {
+  return a - b;
+};
+
+const divide = (a, b) => {
+  return a / b;
+};
+
+const multiply = (a, b) => {
+  return a * b;
+};
+
+const power = (a, b) => {
+  return a ** b;
+};
+
+const max = (arr) => {
+  return arr.sort((a, b) => {
+    return b - a;
+  })[0];
+};
+
+const min = (arr) => {
+  return arr.sort((a, b) => {
+    return a - b;
+  })[0];
+};
+
+const round = (num = 1.56) => {
+  const str = `${num}`;
+  const arr = str.split('.');
+  for (let i = 0; i < arr[1].length; i++) {
+    if (arr[1][i] < 5) break;
+    if (arr[1][i] > 5) {
+      arr[0] = +arr[0] + 1;
+      break;
+    };
+    if (i === arr[1].length - 1) {
+      arr[0] = +arr[0] + 1;
+    };
+  };
+  return +arr[0];
+};
+
+const floor = (num = 1.56) => {
+  const arr = `${num}`.split('.');
+  if (arr.length === 1) return num;
+  return +arr[0];
+};
+
+const ceil = (num = 1.56) => {
+  const arr = `${num}`.split('.');
+  if (arr.length === 1) return num;
+  return +arr[0] + 1;
+};
+
+const percent = (num, perc) => {
+  return (num / 100) * perc;
+};
+
+const isOdd = (num) => {
+  return !!(num % 2);
+};
+
+const isEven = (num) => {
+  return !(num % 2);
+};
+
+const Calculator = {
+  sum: sum,
+  minus: minus,
+  divide: divide,
+  multiply: multiply,
+  power: power,
+  max: max,
+  min: min,
+  round: round,
+  floor: floor,
+  ceil: ceil,
+  percent: percent,
+  isOdd: isOdd,
+  isEven: isEven,
+};
+
